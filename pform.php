d22e4453f067ef21d182b614f2613f9318d1f622
Nimesh Kadecha <nimeshkadecha4560@gmail.com>
2023-03-05 18:52:38 +0530

    Some design added in form

--- pform.php
+++ pform.php
@@ -13,11 +13,14 @@ if (isset($_GET['id'])) {
 
 ?>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
+
+
+
+<!-- <form action="upload.php" method="post" enctype="multipart/form-data">
     <label>Select Image File:</label>
     <input type="file" name="image">
     <input type="submit" name="submit" value="Upload">
-</form>
+</form> -->
 
 <?php
 ?>
